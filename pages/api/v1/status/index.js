d15ca83b1cfb4e38add739756b7bf1f7ba41fec3
Samuel Leite <samuel.lemeleite@gmail.com>
2026-01-11 12:32:38 -0300

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from '../../../../infra/database';
+import database from "./infra/database";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 as status');
+  const result = await database.query("SELECT 1 as status");
   console.log(result.rows);
   response.status(200).json({ status: "é cara vamos ver como vai ser" });
 }
