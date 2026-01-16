9ff7270db011c493f7b4c7502ebed3cecc34d58d
Samuel Leite <samuel.lemeleite@gmail.com>
2026-01-15 23:06:58 -0300

    add a more robost ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV !== "development"
+    ssl: getSSLValues()
   });
   try {
     await client.connect();
@@ -19,6 +19,15 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if(process.env.POSTGRES_CA){
+    return {
+      ca: process.env.POSTGRES_CA //Variável para adicionar o Certificado AutoAssinado (CA)
+    };
+  }
+  return process.env.NODE_ENV !== "development";
 }
 
 export default {
